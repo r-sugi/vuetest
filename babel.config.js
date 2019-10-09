5421eb680c7bb0c2ec1f15f03f0ca0703f55ee56
Ryo Sugimoto <r.sugimoto@gmail.com>
2019-10-09 23:59:27 +0900

    add: first test

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,11 @@
 module.exports = {
   presets: [
+    ["env", { "modules": false }],
     '@vue/app'
-  ]
+  ],
+  "env": {
+    "test": {
+      "presets": [["env", { "targets": { "node": "current" } }]]
+    }
+  }
 }
---
+++ src/components/hoge.spec.js
@@ -0,0 +1,5 @@
+describe('hoge', () => {
+  it('test', () => {
+    expect(1 + 2).toBe(3);
+  })
+})
